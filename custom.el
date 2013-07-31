5bef26474573ec5ff01b7e9a48ff40a8893cbe31
skin-cat <kobamail.88@gmail.com>
2013-08-01 00:27:48 +0800

    added cococ2dx path to semantic include path config

--- custom.el
+++ custom.el
@@ -8,7 +8,7 @@
  '(custom-enabled-themes (quote (deeper-blue)))
  '(ede-project-directories (quote ("/home/alexander/Workspace/C++/Emacs/VictorianRushHour" "/home/alexander/Workspace/C++/Emacs/VictorianRushHour/VictorianRushHour")))
  '(fci-rule-color "#383838")
- '(semantic-c-dependency-system-include-path (quote ("/usr/include")))
+ '(semantic-c-dependency-system-include-path (quote ("/usr/include" "/opt/cocos2d-x/cocos2dx" "/opt/cocos2d-x/cocos2dx/cocoa" "/opt/cocos2d-x/cocos2dx/include" "/opt/cocos2d-x/cocos2dx/kazmath/include" "/opt/cocos2d-x/cocos2dx/platform" "/opt/cocos2d-x/cocos2dx/platform/linux" "/opt/cocos2d-x/cocos2dx/platform/third_party/linux" "/opt/cocos2d-x/cocos2dx/platform/third_party/linux/libfreetype2" "/opt/cocos2d-x/cocos2dx/platform/third_party/linux/libxml2" "/opt/cocos2d-x/cocos2dx/platform/third_party/linux/libjpeg" "/opt/cocos2d-x/cocos2dx/platform/third_party/linux/glew-1.7.0/glew-1.7.0/include" "/opt/cocos2d-x/CocosDenshion/include" "/opt/cocos2d-x/extensions")))
  '(vc-annotate-background "#2B2B2B")
  '(vc-annotate-color-map (quote ((20 . "#BC8383") (40 . "#CC9393") (60 . "#DFAF8F") (80 . "#D0BF8F") (100 . "#E0CF9F") (120 . "#F0DFAF") (140 . "#5F7F5F") (160 . "#7F9F7F") (180 . "#8FB28F") (200 . "#9FC59F") (220 . "#AFD8AF") (240 . "#BFEBBF") (260 . "#93E0E3") (280 . "#6CA0A3") (300 . "#7CB8BB") (320 . "#8CD0D3") (340 . "#94BFF3") (360 . "#DC8CC3"))))
  '(vc-annotate-very-old-color "#DC8CC3"))
