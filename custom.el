e74b56a5b6771cdb816a2ab38463f5396aab08b1
skin-cat <kobamail.88@gmail.com>
2013-08-01 00:11:15 +0800

    changed colot theme to deepper-blue

--- custom.el
+++ custom.el
@@ -4,8 +4,14 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(ac-sources (quote (ac-source-semantic ac-source-abbrev ac-source-words-in-buffer ac-source-files-in-current-dir ac-source-filename)) t)
+ '(ansi-color-names-vector ["#3F3F3F" "#CC9393" "#7F9F7F" "#F0DFAF" "#8CD0D3" "#DC8CC3" "#93E0E3" "#DCDCCC"])
+ '(custom-enabled-themes (quote (deeper-blue)))
  '(ede-project-directories (quote ("/home/alexander/Workspace/C++/Emacs/VictorianRushHour" "/home/alexander/Workspace/C++/Emacs/VictorianRushHour/VictorianRushHour")))
- '(semantic-c-dependency-system-include-path (quote ("/usr/include"))))
+ '(fci-rule-color "#383838")
+ '(semantic-c-dependency-system-include-path (quote ("/usr/include")))
+ '(vc-annotate-background "#2B2B2B")
+ '(vc-annotate-color-map (quote ((20 . "#BC8383") (40 . "#CC9393") (60 . "#DFAF8F") (80 . "#D0BF8F") (100 . "#E0CF9F") (120 . "#F0DFAF") (140 . "#5F7F5F") (160 . "#7F9F7F") (180 . "#8FB28F") (200 . "#9FC59F") (220 . "#AFD8AF") (240 . "#BFEBBF") (260 . "#93E0E3") (280 . "#6CA0A3") (300 . "#7CB8BB") (320 . "#8CD0D3") (340 . "#94BFF3") (360 . "#DC8CC3"))))
+ '(vc-annotate-very-old-color "#DC8CC3"))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
